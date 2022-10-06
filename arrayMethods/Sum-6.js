66f519a6ea4e7dab00b30148e1c397844c787337
Pavel Emelyanov <pavel_emel98@mail.ru>
2022-10-06 21:41:54 +0300

    rewrite

--- arrayMethods/Sum-6.js
+++ arrayMethods/Sum-6.js
@@ -2,8 +2,8 @@ let arr1 = [1, 2, 3, 4, 5];
 let arr2 = [4, 5, 6];
 
 let arr3 = []
-for(let i = 0;i<Math.max(arr1.length,arr2.length);i++){
-    arr3.push((arr1[i] || 0)+(arr2[i] || 0)) 
+const summ = (array1, array2) => {
+    return  array1.map((item, index) => item += array2[index] || 0);
 }
 
-document.writeln(arr3); // [5,7,9,4,5]
+document.writeln(summ); // [5,7,9,4,5]
